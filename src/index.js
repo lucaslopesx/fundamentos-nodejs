3a7200dbcf6e3602fc9ce88e208383baf989928e
Lucas Lopes <lucas.lopesx1@gmail.com>
2022-02-06 21:28:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json());
+
 /*
     GET - Search for info in server
     POST - Insert info in server
@@ -11,24 +13,30 @@ const app = express();
 */
 
 /*
-    Route Params
+    Types of parameters
 
-    Route Params => Identify one resource edit/delete/search
+    Route Params => Identify one resource Edit/Delete/Search
+    Query Params => Pagination/Filter
+    Body Params => Objects for Insertion/Change (JSON)
 */
 
 
 
 app.get("/courses", (req, res)=> {
+    const query = req.query;
+    console.log(query)
     return res.json(["Curso 1", "Curso 2", "Curso 3"])
 })
 
 app.post("/courses", (req, res)=> {
+    const body = req.body;
+    console.log(body)
     return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
 app.put("/courses/:id", (req, res) => {
-    const params = req.params;
-    console.log(params)
+    const { id } = req.params;
+    console.log(id)
     return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
 })
 
